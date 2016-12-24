8f7995a11bb677d1b1b73d4d5694f9d35ebd2ce8
Richard <rahayn@yahoo.com>
2016-12-24 17:38:42 -0500

    Xmas Tree
    Little project I found and refactored for the holidays. Removed the
    option for the user to set the triangles and added a base and stem as
    well as different variable names.

---
+++ christmas_tree1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace christmas_tree1
+{
+	class Christmas_tree
+	{
+		public static void Main(string[] args)
+		{
+			string tree_stump = "      *****";
+			string line_point = "        *";//This is intended to act a the line base for the tree
+			int numTri = 5;
+			for (int i = 1; i <= numTri; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					string tree_branch = new string('*', j);
+					Console.WriteLine(tree_branch.PadLeft(numTri + 3)+ "*"+ tree_branch);
+
+				}
+
+			}
+
+			Console.WriteLine(line_point);
+			Console.WriteLine(line_point);
+			Console.WriteLine(line_point);
+			//TODO fix the above line of code. It's poor practice for there to be this many duplications of code
+
+			Console.WriteLine(tree_stump);
+			Console.WriteLine("Merry Christmas");
+		}
+	}
+
+}
